8a3879b6edacaa0e67b7aafde854224bcdf39bd0
christineoure <ourechristine@gmail.com>
2022-08-10 17:53:50 +0300

    add JS-CAT

---
+++ index.js
@@ -0,0 +1,11 @@
+//  Promise = promise{
+//    resolve:(resolve, reject) => {
+//         if FullName.isEmpty(resolve) === true
+//           return resolve;
+//         else
+//          reject
+//     }
+//  }
+//  console.log(name); 
+
+let name = name.
